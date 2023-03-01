9b15163efa6d852ab188b0363b38c1f75d707669
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-03-01 23:26:55 +0200

    added libraries

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,34 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  String.prototype.toString = function () {
+    return `${this}`;
+  };
+
+  console.log(true.toString());
+
+  // 1. Создал переменную для каждого статуса
+  // const STATUS_BUSY = "busy";
+  // const STATUS_READY = "ready";
+  // // 2. Использовал вычисляемые свойства
+  // const STATUS_LABELS = {
+  //   [STATUS_BUSY]: "Ожидает",
+  //   [STATUS_READY]: "Готов",
+  // };
+  // // 3. Получил массив из БД
+  // const drivers = [
+  //   { name: "Павел", status: "busy" },
+  //   { name: "Сергей", status: "ready" },
+  // ];
+  // // 4. Перевел статусы в нужный (человеческий) формат
+  // const driverStatuses = drivers.map((driver) => {
+  //   const { status } = driver;
+  //   console.log(status);
+  //   return STATUS_LABELS[status];
+  // });
+  // console.log(driverStatuses);
+  // // Array [ "Ожидает", "Готов" ]
+
   return (
     <div className="App">
       <div>
